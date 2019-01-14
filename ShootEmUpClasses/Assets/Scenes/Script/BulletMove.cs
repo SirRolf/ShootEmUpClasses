aca54978b68c921bfab6c2ec6b79cfa1d861db34
Floris <Flomo123456789@gmail.com>
2019-01-14 09:18:06 +0100

    enemy and bullet movement

---
+++ ShootEmUpClasses/Assets/Scenes/Script/BulletMove.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletMove : MonoBehaviour {
+
+	void Update () {
+        transform.Translate(Vector3.right * 4 * Time.deltaTime);
+    }
+}
---
+++ ShootEmUpClasses/Assets/Scenes/Script/EnemyCollision.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyCollision : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject enemy;
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Bullet")
+        {
+            Instantiate(enemy, transform.position, transform.rotation);
+            Destroy(this.gameObject);
+            Destroy(col.gameObject);
+        }
+    }
+}
